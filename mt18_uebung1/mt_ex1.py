70a0c5bd1388d316551de6169764b25889a804e4
zurichind <lucaelio.salini@uzh.ch>
2018-03-11 17:34:51 +0100

    Revert "aded"
    This reverts commit c2ad719e662a75d7b94bf83b9afbb9992fb5235b.

--- mt18_uebung1/mt_ex1.py
+++ mt18_uebung1/mt_ex1.py
@@ -1,18 +1,15 @@
 # Albina und Luca
 
 import nltk
-import itertools
 from math import exp
 
 def open_and_tokenize(input):
     """Reads in file by its location and returns a list of the tokenized text."""
 
     out = []
-
     with open(input, 'r') as infile:
         for counter, line in enumerate(infile):
             out.append(nltk.word_tokenize(line))
-
     return out
 
 
@@ -41,10 +38,8 @@ def compute_ngram_precision(hypothesis, reference, ngram_nr=2):
     #print(length, '\n\n')
     return counter/length
 
-
 def compute_BLEU(hypothesis, reference, ngram_nr=2):
     """Computes the BLEU Score of a hypothetical translation with the help of a reference sentence"""
-
     product = 1
     N = ngram_nr
 
@@ -55,31 +50,15 @@ def compute_BLEU(hypothesis, reference, ngram_nr=2):
     ngram_prec = product ** (1/N)
     # print(ngram_prec)
 
-    brevity_penalty = min([1, exp(1 - (len(reference) / len(hypothesis)))])
+    brevity_penalty = min([1, exp(1 - (len(nltk.word_tokenize(reference)) / len(nltk.word_tokenize(hypothesis))))])
     # print(brevity_penalty)
 
     return brevity_penalty * ngram_prec
 
 
-def main():
-    """Just a main function, as always :)"""
-
-    # hypo = 'airport security Israeli officials are responsible'
-    # ref = 'Israeli officials are responsible for airport security'
-    # print(compute_BLEU(hypo, ref, 4))
-
-    BLEU = 0
-
-    hypofile = str(input('Hi there! Please type in the file path to your translated text file: '))
-    reffile = str(input('And now type in the file path to your reference text file: '))
-
-    tokenized_hypo = open_and_tokenize(hypofile)
-    tokenized_ref = open_and_tokenize(reffile)
-
-    for hypo, ref in list(zip(tokenized_hypo, tokenized_ref)):
-        print(hypo, ref)
-        print(compute_BLEU(hypo, ref, 4))
-
-
 if __name__ == '__main__':
-    main()
+    hypo = 'airport security Israeli officials are responsible'
+    ref = 'Israeli officials are responsible for airport security'
+
+    #print(open_and_tokenize(r'C:\Users\salin\Google Drive\Uni Zurich\4 Semester\Maschinelle Übersetzung\Übungen\MT-2018\mt18_uebung1\artikel_zeit\text_de.txt'))
+    print(compute_BLEU(hypo, ref, 4))
