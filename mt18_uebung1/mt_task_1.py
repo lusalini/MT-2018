8fdfd45dc7d72af764d3737fddb05a055403f692
zurichind <lucaelio.salini@uzh.ch>
2018-03-12 11:08:26 +0100

    finished assignment 1

--- mt18_uebung1/mt_task_1.py
+++ mt18_uebung1/mt_task_1.py
@@ -14,8 +14,10 @@ import sys
 
 def tokenize_file(input:str) -> list:
     """ Opens the input file for reading and makes a list of tokenized input"""
+
     with open(input, 'r') as infile:
         tokenized = nltk.word_tokenize(infile.read())
+
         return tokenized
 
 
@@ -25,19 +27,21 @@ def compute_ngram_precision(hypothesis, reference, ngram_nr):
     ngrams_hyp = list(ngrams(hypothesis, ngram_nr))
     ngrams_ref = list(ngrams(reference, ngram_nr))
     hyp_length = len(ngrams_hyp)
-    ref_length = len(ngrams_ref)
     correct = 0
+
     for item in ngrams_hyp:
         if item in ngrams_ref:
             correct += 1
             ngrams_ref.remove(item)
+
     p = correct / hyp_length
-    #print(p)
+
     return p
 
 
 def compute_bleu_score(hyp, ref, ngram_nr):
     """Computes the BLEU Score of a hypothetical translation with the help of a reference sentence"""
+
     product = 1
     N = ngram_nr
 
@@ -46,10 +50,10 @@ def compute_bleu_score(hyp, ref, ngram_nr):
         ngram_nr -= 1
 
     P = product ** (1 / N)
-    #print(P)
     brevity_penalty = min(1.0, np.exp(1 - (len(ref)/len(hyp))))
     bleu = P * brevity_penalty
-    print('%.3f' % bleu)
+
+    return bleu
 
 
 def main():
@@ -62,6 +66,8 @@ def main():
     precision = compute_ngram_precision(hyp, ref, 1)
     bleu = compute_bleu_score(hyp, ref, 4)
 
+    print('%.3f' % bleu)
+
+
 if __name__ == '__main__':
     main()
-
